399aa44ecfdd449746f8677821db0672dc7681aa
abimelecmartinez <enoc.martinez@pulzo.com>
2023-10-03 12:01:01 -0500

    :wrench: Deshabilita temporalmente Sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,10 +36,10 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            if (app()->bound('sentry')) {
-                app('sentry')->captureException($e);
-              }
-        });
+        // $this->reportable(function (Throwable $e) {
+        //     if (app()->bound('sentry')) {
+        //         app('sentry')->captureException($e);
+        //       }
+        // });
     }
 }
