4da290e683a115eaf574852a1572d047d3f713df
abimelecmartinez <enoc.martinez@pulzo.com>
2024-02-16 14:14:03 -0500

    :sparkles: Muestra el paginador únicamente si supera el número de items por página

--- resources/views/paginator.blade.php
+++ resources/views/paginator.blade.php
@@ -1,54 +1,51 @@
-@if ($paginate->TotalRecords() > 0) 
+@if ($paginate->TotalRecords() > env('APP_PAGINADOR_NUM_ITEMS')) 
+
+@php 
+  $dataPrevious = [$paginate->Previous()];
+  $dataNext = [$paginate->Next()];
+
+  if (strlen($criterio)>0) {
+    array_push($dataPrevious, $criterio);
+    array_push($dataNext, $criterio);
+  }
+@endphp
 
   <div class="block-content mt-0">
       <nav aria-label="Page navigation">
         <div class="pagination pagination-sm justify-content-center">
 
           @if (!$paginate->IsFirst())   
-              @if (strlen($criterio)>0)
-              <a class="page-link" href="{{ route($route, [$paginate->Previous(), $criterio]) }}" aria-label="Previous">
-                <span aria-hidden="true">
-                  <i class="fa fa-angle-left mb-2 mt-1 p-1 text-muted"></i>
-                </span>
-              </a>              
-              @else
-              <a class="page-link" href="{{ route($route, $paginate->Previous()) }}" aria-label="Previous">
+              <a class="page-link" href="{{ route($route, $dataPrevious) }}" aria-label="Previous">
                 <span aria-hidden="true">
                   <i class="fa fa-angle-left mb-2 mt-1 p-1 text-muted"></i>
                 </span>
               </a>  
-              @endif 
           @endif
 
           <span class="text-muted fs-sm mb-2 mt-1 p-1">Página</span>
           <div class="btn-toolbar mb-1" role="toolbar" aria-label="Paginador">
             <select class="form-select fs-sm text-muted" id="page" name="page" onchange="paginate()">
               @for($i=1; $i <= $paginate->NumberOfPages(); $i++)
-                @if (strlen($criterio)>0)
-                  <option value="{{ route($route, [$i, $criterio]) }}" {{ ($i == $paginate->Page() ? 'selected' : '') }}>{{ $i }}</option>
-                @else
-                  <option value="{{ route($route, $i) }}" {{ ($i == $paginate->Page() ? 'selected' : '') }}>{{ $i }}</option>
-                @endif
+                @php
+                  $data = [$i];
+                  if (strlen($criterio)>0) {
+                    array_push($data, $criterio);
+                  }                  
+                @endphp
+
+                <option value="{{ route($route, $data) }}" {{ ($i == $paginate->Page() ? 'selected' : '') }}>{{ $i }}</option>
+
               @endfor
             </select>          
           </div>
           <span class="text-muted fs-sm mb-2 mt-1 p-1">de {{ $paginate->NumberOfPages() }} página(s)</span>
 
           @if (!$paginate->IsLast())  
-            @if (strlen($criterio)>0)
-              <a class="page-link" href="{{ route($route, [$paginate->Next(), $criterio]) }}" aria-label="Next">
+              <a class="page-link" href="{{ route($route, $dataNext) }}" aria-label="Next">
                 <span aria-hidden="true">
                   <i class="fa fa-angle-right mb-2 mt-1 p-1 text-muted"></i>
                 </span>
               </a>            
-            @else
-              <a class="page-link" href="{{ route($route, $paginate->Next(), $criterio) }}" aria-label="Next">
-                <span aria-hidden="true">
-                  <i class="fa fa-angle-right mb-2 mt-1 p-1 text-muted"></i>
-                </span>
-              </a>            
-            @endif
-    
           @endif
           <!-- <div class="pagination pagination-sm justify-content-end text-muted fs-sm mb-2 mt-1 p-1"> {{ $paginate->TotalRecords() }} registros</div>         -->
         </div> 
