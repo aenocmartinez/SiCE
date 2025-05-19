30da50205ce7fe6afc9617cd798ca7359307e054
Abimelec Martinez <aenoc.martinez@gmail.com>
2025-05-18 19:35:55 -0500

    :wrench: Retira mensaje .env y configurar path de libreOffice para Linux

--- src/usecase/certificados/GenerarCertificadoWordUseCase.php
+++ src/usecase/certificados/GenerarCertificadoWordUseCase.php
@@ -93,9 +93,9 @@ class GenerarCertificadoWordUseCase
 
             $template->saveAs($rutaDocx);
 
-            dd("Mensaje");
             // Verificar si se debe convertir a PDF
             if (env('CERTIFICADO_CONVERTIR_PDF', false)) {
+                dd("Entra");
                 // $libreOfficePath = env('LIBREOFFICE_PATH', 'C:\\Program Files\\LibreOffice\\program\\soffice.exe'); 
                 $libreOfficePath = env('LIBREOFFICE_PATH') ?? '/usr/bin/libreoffice';
 
