10c0560e94c00b2882bddd080ad7443089b6770a
abimelecmartinez <enoc.martinez@pulzo.com>
2025-05-29 07:24:59 -0500

    :recycle: Mejora función ObtenerRutaRelativa

--- src/infraestructure/util/URL.php
+++ src/infraestructure/util/URL.php
@@ -20,13 +20,9 @@ class URL
     {
         $ruta = parse_url($path, PHP_URL_PATH);
 
-        $rutaRelativa = str_replace(['/storage/app/public/', '/storage/public/'], '/storage/', $ruta);
+        $nombreArchivo = basename($ruta); 
 
-        $rutaRelativa = str_replace('/app/public/', '/storage/', $rutaRelativa);
-
-        $rutaRelativa = preg_replace('#/+#', '/', $rutaRelativa);
-
-        return $rutaRelativa;
+        return '/storage/pdfs/' . $nombreArchivo;
     }
 
 }
